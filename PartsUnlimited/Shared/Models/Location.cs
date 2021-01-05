9708aeb5d055e3c29528b1465b40e7b92e569f1a
wstonedev <wstonedev@outlook.com>
2021-01-06 00:06:58 +0800

    update

---
+++ PartsUnlimited/Shared/Models/Location.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartsUnlimited.Shared.Models
+{
+    public class Location
+    {
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+}
